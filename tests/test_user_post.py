0d8c35189421f0cef7356ea9769b8a41173d9aff
Vitalii <vitalikbashkiser@gmail.com>
2024-08-07 01:34:12 +0300

    add tests to my project

--- tests/test_user_post.py
+++ tests/test_user_post.py
@@ -2,107 +2,177 @@ from datetime import datetime, timedelta
 
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+from base import Base
 from main import app
-from db.session import SessionLocal
-from post import models
+from user.models import User
+from post.models import Post, Comment
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test_db.sqlite3"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine
+)
 
 client = TestClient(app)
 
 
+@pytest.fixture(scope="module", autouse=True)
+def setup_and_teardown():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+
 @pytest.fixture
 def create_test_user():
-    db = SessionLocal()
-    user = models.User(email="test@example.com", username="testuser", hashed_password="hashedpassword")
-    db.add(user)
-    db.commit()
-    db.refresh(user)
+    db = TestingSessionLocal()
+    user = db.query(User).filter(User.email == "testuser@example.com").first()
+    if not user:
+        user = User(
+            email="testuser@example.com", hashed_password="hashedpassword123"
+        )
+        db.add(user)
+        db.commit()
     db.close()
-    return user
 
-
-@pytest.fixture
-def create_test_post(create_test_user):
-    db = SessionLocal()
-    post = models.Post(title="Test Post", content="Test Content", user_id=create_test_user.id)
-    db.add(post)
-    db.commit()
-    db.refresh(post)
-    db.close()
-    return post
-
-
-def test_create_post(create_test_user):
     response = client.post(
-        "/create/",
+        "/user/login",
+        json={"email": "testuser@example.com", "password": "testpassword"},
+    )
+    token = response.json().get("access_token")
+    return user, token
+
+
+def test_register_user(create_test_user):
+    response = client.post(
+        "/user/signup",
+        json={"email": "uniqueuser@example.com", "password": "uniquepassword"},
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert (
+        "access_token" in data or data.get("detail") == "Email already in use"
+    )
+
+
+def test_user_login(create_test_user):
+    _, token = create_test_user
+    response = client.post(
+        "/user/login",
+        json={"email": "uniqueuser@example.com", "password": "uniquepassword"},
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert "access_token" in data
+
+
+def test_create_new_post(create_test_user):
+    _, token = create_test_user
+
+    response = client.post(
+        "/posts/",
         json={
-            "title": "New Post",
-            "content": "Content of the new post",
-            "user_id": create_test_user.id,
+            "title": "Sample Title",
+            "content": "Sample Content",
+            "user_id": 1,
         },
+        headers={"Authorization": f"Bearer {token}"},
     )
-    assert response.status_code == 200
+    assert response.status_code == 200, response.json()
     data = response.json()
-    assert data["title"] == "New Post"
-    assert data["content"] == "Content of the new post"
+    assert data["title"] == "Sample Title"
+    assert data["content"] == "Sample Content"
+
+    db = TestingSessionLocal()
+    post = db.query(Post).filter(Post.title == "Sample Title").first()
+    if post:
+        db.delete(post)
+        db.commit()
+    db.close()
 
 
-def test_read_posts():
-    response = client.get("/posts/")
-    assert response.status_code == 200
-    data = response.json()
-    assert isinstance(data, list)
-    assert len(data) > 0
+def test_comments_breakdown(create_test_user):
+    user, token = create_test_user
 
+    today_date = datetime.now().strftime("%Y-%m-%d")
+    next_day_date = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
 
-def test_read_post(create_test_post):
-    response = client.get(f"/posts/{create_test_post.id}")
-    assert response.status_code == 200
-    data = response.json()
-    assert data["title"] == create_test_post.title
+    db = TestingSessionLocal()
+    post = Post(title="Sample Post", content="Sample Content", user_id=user.id)
+    db.add(post)
 
-
-def test_update_post(create_test_post):
-    response = client.put(
-        f"/posts/{create_test_post.id}",
-        json={"title": "Updated Title", "content": "Updated Content"},
+    comment1 = Comment(
+        content="Positive comment",
+        post_id=post.id,
+        created_at=datetime.now(),
+        blocked=False,
     )
-    assert response.status_code == 200
-    data = response.json()
-    assert data["title"] == "Updated Title"
+    db.add(comment1)
 
-
-def test_delete_post(create_test_post):
-    response = client.delete(f"/posts/{create_test_post.id}")
-    assert response.status_code == 200
-    data = response.json()
-    assert data["title"] == create_test_post.title
-
-
-def test_create_comment(create_test_post, create_test_user):
-    response = client.post(
-        f"/comments/{create_test_post.id}",
-        json={"content": "This is a comment", "user_id": create_test_user.id},
+    comment2 = Comment(
+        content="Negative comment with inappropriate content",
+        post_id=post.id,
+        created_at=datetime.now(),
+        blocked=True,
     )
-    assert response.status_code == 200
-    data = response.json()
-    assert data["content"] == "This is a comment"
+    db.add(comment2)
 
+    db.commit()
 
-def test_read_comments():
-    response = client.get("/comments/")
-    assert response.status_code == 200
-    data = response.json()
-    assert isinstance(data, list)
+    comments = db.query(Comment).all()
+    assert len(comments) >= 2
 
-
-def test_get_comments_daily_breakdown():
-    date_from = datetime.now().strftime("%Y-%m-%d")
-    date_to = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
     response = client.get(
         "/comments-daily-breakdown",
-        params={"date_from": date_from, "date_to": date_to},
+        params={"date_from": today_date, "date_to": next_day_date},
+        headers={"Authorization": f"Bearer {token}"},
     )
+
     assert response.status_code == 200
     data = response.json()
-    assert isinstance(data, list)
+    assert "total_comments" in data[0]
+    assert "blocked_comments" in data[0]
+    assert data[0]["blocked_comments"] >= 1
+
+    comment1 = (
+        db.query(Comment).filter(Comment.content == "Positive comment").first()
+    )
+    if comment1:
+        db.delete(comment1)
+
+    comment2 = (
+        db.query(Comment)
+        .filter(
+            Comment.content == "Negative comment with inappropriate content"
+        )
+        .first()
+    )
+    if comment2:
+        db.delete(comment2)
+
+    post = db.query(Post).filter(Post.title == "Sample Post").first()
+    if post:
+        db.delete(post)
+
+    db.commit()
+    db.close()
+
+
+def test_access_protected_route(create_test_user):
+    _, token = create_test_user
+
+    response = client.get(
+        "/posts/", headers={"Authorization": f"Bearer {token}"}
+    )
+    assert response.status_code == 200
+
+
+def test_access_protected_route_without_token():
+    response = client.get("/posts/")
+    assert response.status_code == 403
